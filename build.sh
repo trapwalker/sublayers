ac01909273cdaa70c0addbca90e29f398c620123
svp <svpmailbox@gmail.com>
2020-03-04 13:16:40 +0300

    github pages adopt

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+markdown Home.md                    > docs/index.html
+markdown about.md                   > docs/about.html
+markdown activity.md                > docs/activity.html
+markdown build.sh                   > docs/build.html
+markdown dd.md                      > docs/dd.html
+markdown features.md                > docs/features.html
+markdown foreign_resources.md       > docs/foreign_resources.html
+markdown interface.md               > docs/interface.html
+markdown plan.md                    > docs/plan.html
+markdown story.md                   > docs/story.html
+markdown "Vision & Scope.md"        > docs/vision.html
+markdown "О Sublayers коротко.md"   > docs/short.html
